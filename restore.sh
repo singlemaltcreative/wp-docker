08b4057d8725333d10cf1556dfc1f1b8d71d6f0c
Paul Bredenberg <pbredenberg@Pauls-MacBook-Pro.local>
2017-02-25 08:40:44 -0500

    use beter method for retrieving mysql container id

--- restore.sh
+++ restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 DBNAME=wordpress
-MYSQL=$(docker-compose ps | grep mysql | awk '{ print $1 }')
+MYSQL=$(docker-compose ps -q mysql)
 docker exec $MYSQL /bin/bash -c "mysql -u root -ppassword $DBNAME < /var/lib/mysql/database.sql"
